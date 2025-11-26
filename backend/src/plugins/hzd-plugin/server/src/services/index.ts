eadb1dd00036c4406fc39307ebbe6cd973d02d41
Tilo Thiele <tilo.thiele@emessages.de>
2025-11-26 14:18:52 +0100

    fixes

--- backend/src/plugins/hzd-plugin/server/src/services/index.ts
+++ backend/src/plugins/hzd-plugin/server/src/services/index.ts
@@ -2,9 +2,11 @@ import service from './service';
 import dog from '../../services/dog';
 import litter from '../../services/litter';
 import breeder from '../../services/breeder';
+import member from '../../services/member';
 export default {
   service,
   dog,
   breeder,
   litter,
+  member,
 };
