ec7d6ab37dfb788229752f243d380dd68e708c6f
Tilo Thiele <tilo.thiele@emessages.de>
2025-11-26 14:32:22 +0100

    member route/controller fixed

--- backend/src/plugins/hzd-plugin/server/src/controllers/index.ts
+++ backend/src/plugins/hzd-plugin/server/src/controllers/index.ts
@@ -2,10 +2,12 @@ import controller from './controller';
 import dog from './dog';
 import breeder from './breeder';
 import litter from './litter';
+import member from './member';
 
 export default {
   controller,
   dog,
   breeder,
   litter,
+  member,
 };
