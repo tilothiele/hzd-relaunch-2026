922fd1408ea9df9748423b5425a0c4871f27718b
Tilo Thiele <tilo.thiele@emessages.de>
2025-12-09 13:16:42 +0100

    added nodemailer

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,9 +1,26 @@
-export default () => ({
+export default ({env}) => ({
   'hzd-plugin': {
     enabled: true,
     resolve: './src/plugins/hzd-plugin'
   },
-
+ email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env('SMTP_DEFAULT_FROM', 't.thiele@hovawarte.com'),
+        defaultReplyTo:env('SMTP_DEFAULT_REPLY_TO', 't.thiele@hovawarte.com'),
+      },
+    },
+  },
   // https://docs.strapi.io/cms/plugins/graphql
   graphql: {
     config: {
