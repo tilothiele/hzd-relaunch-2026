9a9e539d785e3dfddfb8a9add33d8ca1d3416f5f
Tilo thiele <tilo.thiele@hamburg.de>
2026-01-22 16:02:17 +0000

    duplicati

---
+++ resources/maintenance/dump-pgsql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#
+
+. .env
+
+TARGET_DIR="/local-backups/hzd-backend-prod"
+echo "dump postgresql database"
+dump_db() {
+    echo "--- Erstelle Dump der PROD-DB ---"
+    mkdir -p transfer
+    docker run --rm -it -e PGPASSWORD="$PG_PASSWORD" --network "$PG_NETWORK" \
+      -v $TARGET_DIR:/transfer \
+      pgvector/pgvector:pg17 \
+      pg_dump -h "$PG_HOST" -U "$PG_USER" -d "$PG_PROD_DB" -f "/transfer/$DUMP_FILE"
+}
+
+mkdir -p $TARGET_DIR
+dump_db
