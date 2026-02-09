3c3bd8489e94102a9877cb0a4c28d7fd941ab33b
Tilo Thiele <tilo.thiele@emessages.de>
2026-02-09 14:03:42 +0100

    added hzd-app

---
+++ hzd-pwa/next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
+
+const pwaConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // register: true,
+  // skipWaiting: true,
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "standalone",
+};
+
+export default pwaConfig(nextConfig);
