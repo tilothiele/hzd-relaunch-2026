ffa827789e4acaecb57c61d3aab7352ab2652757
Tilo Thiele <tilo.thiele@emessages.de>
2026-02-11 16:18:20 +0100

    update dog.location

--- backend/src/plugins/hzd-plugin/server/src/content-types/index.ts
+++ backend/src/plugins/hzd-plugin/server/src/content-types/index.ts
@@ -11,14 +11,53 @@ import contact from "../../content-types/contact/schema.json"
 // ContentTypeDefinition
 
 // https://docs.strapi.io/cms/backend-customization/models#lifecycle-hooks
-const dogLifecyles = {
+const dogLifecycles = {
+    async beforeCreate(event) {
+        const { data } = event.params;
 
+        if (data.owner) {
+            const ownerId = typeof data.owner === 'object' ? data.owner.id : data.owner;
+            const owner = await strapi.entityService.findOne('plugin::users-permissions.user', ownerId, {
+                populate: ['geoLocation']
+            });
+            if (owner && owner.geoLocation) {
+                const { id, ...geoData } = owner.geoLocation;
+                data.Location = geoData;
+            }
+        }
+    },
+    async beforeUpdate(event) {
+        const { data, where } = event.params;
+
+        let ownerId;
+        if (data.owner) {
+            ownerId = typeof data.owner === 'object' ? data.owner.id : data.owner;
+        } else {
+            // additional lookup if owner is not in the payload
+            const dog = await strapi.entityService.findOne('plugin::hzd-plugin.dog', where.id, {
+                populate: ['owner']
+            });
+            if (dog && dog.owner) {
+                ownerId = dog.owner.id;
+            }
+        }
+
+        if (ownerId) {
+            const owner = await strapi.entityService.findOne('plugin::users-permissions.user', ownerId, {
+                populate: ['geoLocation']
+            });
+            if (owner && owner.geoLocation) {
+                const { id, ...geoData } = owner.geoLocation;
+                data.Location = geoData;
+            }
+        }
+    }
 }
 
 export default {
     dog: {
         schema: dog,
-        lifecyles: dogLifecyles
+        lifecycles: dogLifecycles
     },
     breeder: {
         schema: breeder
