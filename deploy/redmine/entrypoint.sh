7f8ff422a24ca542ff05da5ebbc0f74c034777e9
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-06 22:33:07 +0100

    mail handler

--- deploy/redmine/entrypoint.sh
+++ deploy/redmine/entrypoint.sh
@@ -9,8 +9,10 @@ CONFIG_FILE="/usr/src/redmine/config/configuration.yml"
 
 envsubst < /configuration.yml.template > "$CONFIG_FILE"
 
-CMD="*/5 * * * * rake -f ./Rakefile --silent redmine:email:receive_imap RAILS_ENV="production" host=$SUPPORT_MAIL_HOST username=$SUPPORT_MAIL_USERNAME password=\"$SUPPORT_MAIL_PASSWORD\" unknown_user=create project=support allow_override=from no_permission_check=1 no_account_notice=1"
+CMD="*/2 * * * * cd /usr/src/redmine && /usr/local/bin/bundle exec rake -f ./Rakefile redmine:email:receive_imap RAILS_ENV="production" host=$SUPPORT_MAIL_HOST username=$SUPPORT_MAIL_USERNAME password=\"$SUPPORT_MAIL_PASSWORD\" unknown_user=create project=support allow_override=from no_permission_check=1 no_account_notice=1"
 crontab -u redmine -l 2>/dev/null | { cat; echo "$CMD"; } | crontab -u redmine -
+
+service cron start
 
 exec "$@"
 
