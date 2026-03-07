55f4fc4362465988fb3ef65c2361bca16596b9d0
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-07 12:50:42 +0100

    receive_imap_via_cron

--- deploy/redmine/entrypoint.sh
+++ deploy/redmine/entrypoint.sh
@@ -2,12 +2,14 @@
 set -e
 
 DB_CONFIG_FILE="/usr/src/redmine/config/database.yml"
-
 envsubst < /database.yml.template > "$DB_CONFIG_FILE"
 
 CONFIG_FILE="/usr/src/redmine/config/configuration.yml"
-
 envsubst < /configuration.yml.template > "$CONFIG_FILE"
+
+RECEIVE_IMAP_FILE="/usr/src/redmine/receive_imap.sh"
+envsubst < /receive_imap.sh.template > "$RECEIVE_IMAP_FILE"
+chmod +x "$RECEIVE_IMAP_FILE"
 
 CMD="* * * * * /bin/bash /usr/src/redmine/receive_imap.sh >> /var/log/cron.log 2>&1"
 crontab -u redmine -l 2>/dev/null | { cat; echo "$CMD"; } | crontab -u redmine -
