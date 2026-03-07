0fe6b953f7ff582cf298fdf4f6d267e945e23687
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-07 21:04:25 +0100

    fix-build

--- deploy/redmine/redmine_plugins/project_mail_override/config/routes.rb
+++ deploy/redmine/redmine_plugins/project_mail_override/config/routes.rb
@@ -1,32 +1,9 @@
-# app/controllers/project_mail_settings_controller.rb
+# config/routes.rb
 
-class ProjectMailSettingsController < ApplicationController
-  before_action :find_project
-  before_action :authorize
-
-  def edit
-    @mail_setting = @project.project_mail_setting || @project.build_project_mail_setting
-  end
-
-  def update
-    @mail_setting = @project.project_mail_setting || @project.build_project_mail_setting
-
-    if @mail_setting.update(mail_setting_params)
-      flash[:notice] = 'Mail settings updated'
-      redirect_to settings_project_path(@project, tab: 'mail_override')
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def find_project
-    @project = Project.find(params[:project_id])
-  end
-
-  def mail_setting_params
-    params.require(:project_mail_setting)
-          .permit(:from_address, :reply_to_address)
+Rails.application.routes.draw do
+  resources :projects do
+    resource :mail_settings,
+             controller: 'project_mail_settings',
+             only: [:edit, :update]
   end
 end
