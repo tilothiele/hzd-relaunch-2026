03522ea5cb8cc77c53cd3fec7b05de90f757a7ed
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-07 21:15:21 +0100

    fix-build

--- deploy/redmine/redmine_plugins/project_mail_override/lib/project_mail_override/hooks.rb
+++ deploy/redmine/redmine_plugins/project_mail_override/lib/project_mail_override/hooks.rb
@@ -1,5 +1,10 @@
+# plugins/project_mail_override/lib/project_mail_override/hooks.rb
+
 module ProjectMailOverride
-  class ProjectSettingsHook < Redmine::Hook::ViewListener
-    render_on :view_projects_settings_tabs, partial: 'project_mail_settings/form', locals: {}
+  module Hooks
+    class ProjectSettingsHook < Redmine::Hook::ViewListener
+      render_on :view_projects_settings_tabs,
+                partial: 'project_mail_settings/form'
+    end
   end
 end
