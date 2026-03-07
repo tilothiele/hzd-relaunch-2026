de7ecb9bb416a56475150a644f4300a985a9ed71
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-07 21:51:52 +0100

    view

--- deploy/redmine/redmine_plugins/project_mail_override/app/controllers/project_mail_settings_controller.rb
+++ deploy/redmine/redmine_plugins/project_mail_override/app/controllers/project_mail_settings_controller.rb
@@ -1,32 +1,20 @@
-# app/controllers/project_mail_settings_controller.rb
-
 class ProjectMailSettingsController < ApplicationController
   before_action :find_project
   before_action :authorize
 
-  def edit
-    @mail_setting = @project.project_mail_setting || @project.build_project_mail_setting
-  end
-
   def update
-    @mail_setting = @project.project_mail_setting || @project.build_project_mail_setting
+    setting = @project.project_mail_setting || @project.build_project_mail_setting
 
-    if @mail_setting.update(mail_setting_params)
+    if setting.update(params.require(:project_mail_setting).permit(:from_address, :reply_to_address))
       flash[:notice] = 'Mail settings updated'
-      redirect_to settings_project_path(@project, tab: 'mail_override')
-    else
-      render :edit
     end
+
+    redirect_to settings_project_path(@project, tab: 'mail_override')
   end
 
   private
 
   def find_project
     @project = Project.find(params[:project_id])
-  end
-
-  def mail_setting_params
-    params.require(:project_mail_setting)
-          .permit(:from_address, :reply_to_address)
   end
 end
