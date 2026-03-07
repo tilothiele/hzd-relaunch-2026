a4cca44c12895047bc0b25f12bdde9ea69065e72
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-07 22:00:57 +0100

    view

--- deploy/redmine/redmine_plugins/project_mail_override/lib/project_mail_override/hooks.rb
+++ deploy/redmine/redmine_plugins/project_mail_override/lib/project_mail_override/hooks.rb
@@ -2,13 +2,16 @@ module ProjectMailOverride
   module Hooks
     class ProjectSettingsHook < Redmine::Hook::ViewListener
       def view_projects_settings_tabs(context = {})
-        context[:tabs] << {
-          name: 'mail_override',
-          label: 'Mail Override',
-          action: :manage_project_mail_settings,
-          partial: 'project_mail_settings/settings'
-        }
-      end
+        # Sicherstellen, dass tabs existiert
+        tabs = context[:tabs] ||= []
+
+        # Neuen Tab hinzufügen
+        tabs << {
+          name: 'mail_override',                               # interne ID des Tabs
+          label: 'Mail Override',                              # Beschriftung im UI
+          action: :manage_project_mail_settings,              # Permission-Name
+          partial: 'project_mail_settings/settings'           # Partial, das gerendert wird
+        }      end
     end
   end
 end
