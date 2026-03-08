cff02a06e02e2c08d502f2ad3e5fa7eaad970e3f
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-08 01:23:11 +0100

    add verbose puts to issue patch

--- deploy/redmine/redmine_plugins/project_mail_override/lib/project_mail_override/patches/issue_patch.rb
+++ deploy/redmine/redmine_plugins/project_mail_override/lib/project_mail_override/patches/issue_patch.rb
@@ -11,23 +11,40 @@ module ProjectMailOverride
       private
 
       def send_project_mail_override_notification
+        puts "--- [ProjectMailOverride] send_project_mail_override_notification for Issue ##{id} ---"
+        
         # Check if project and module is enabled
-        return unless project && project.module_enabled?(:project_mail_override)
+        unless project && project.module_enabled?(:project_mail_override)
+          puts "--- [ProjectMailOverride] Project or module not enabled for Issue ##{id} ---"
+          return
+        end
 
         # Check setting
         setting = project.project_mail_setting
-        return unless setting && setting.send_notification_on_create?
+        unless setting
+          puts "--- [ProjectMailOverride] No ProjectMailSetting record found for project #{project.identifier} ---"
+          return
+        end
+
+        puts "--- [ProjectMailOverride] Setting send_notification_on_create is: #{setting.send_notification_on_create} ---"
+        unless setting.send_notification_on_create?
+          puts "--- [ProjectMailOverride] send_notification_on_create is FALSE, skipping ---"
+          return
+        end
 
         # Send notification to author
         if author && author.active?
-          Rails.logger.info "[ProjectMailOverride] Sending creation notification for Issue ##{id} to #{author.login}"
+          puts "--- [ProjectMailOverride] Triggering Mailer.issue_add for creator: #{author.login} ---"
           begin
             # Use self as the issue object
             Mailer.issue_add(self, [author], []).deliver_now
-            Rails.logger.info "[ProjectMailOverride] Notification delivered"
+            puts "--- [ProjectMailOverride] Mailer.issue_add.deliver_now finished ---"
           rescue => e
+            puts "--- [ProjectMailOverride] CRITICAL ERROR sending notification: #{e.message} ---"
             Rails.logger.error "[ProjectMailOverride] Error sending notification: #{e.message}"
           end
+        else
+          puts "--- [ProjectMailOverride] Author not found or not active: #{author&.login} ---"
         end
       end
     end
