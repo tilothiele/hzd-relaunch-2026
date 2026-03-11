31fc180ab4b1a5aa2181d7fbfb913da5c3dbc37e
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-11 01:55:29 +0100

    resourcespace

--- deploy/resourcespace/entrypoint.sh
+++ deploy/resourcespace/entrypoint.sh
@@ -6,5 +6,7 @@ service cron start
 # Ensure daily cron jobs are executable
 chmod +x /etc/cron.daily/*
 
+envsubst < /config.php.template > /var/www/html/include/config.php
+
 # Start Apache in the foreground (keeps the container alive)
 apachectl -D FOREGROUND
