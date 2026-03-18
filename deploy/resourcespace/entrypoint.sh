49fb1b028cff34802dfa89e69cf80708469ef2f6
Tilo Thiele <tilo.thiele@emessages.de>
2026-03-18 23:56:27 +0100

    fix

--- deploy/resourcespace/entrypoint.sh
+++ deploy/resourcespace/entrypoint.sh
@@ -6,7 +6,6 @@ service cron start
 # Ensure daily cron jobs are executable
 chmod +x /etc/cron.daily/*
 
-envsubst '${MYSQL_HOST} ${MYSQL_USER} ${MYSQL_PASS} ${MYSQL_DB} ${SERVICE_URL_RESOURCESPACE} ${EMAIL_NOTIFY} ${EMAIL_FROM} ${SCRAMBLE_KEY} ${API_SCRAMBLE_KEY}' < /config.php.template > /var/www/html/include/config.php
-
+envsubst '${MYSQL_HOST} ${MYSQL_USER} ${MYSQL_PASS} ${MYSQL_DB} ${SERVICE_URL_RESOURCESPACE} ${EMAIL_NOTIFY} ${EMAIL_FROM} ${SCRAMBLE_KEY} ${API_SCRAMBLE_KEY} ${SMTP_SECURE} ${SMTP_HOST} ${SMTP_PORT} ${SMTP_USERNAME} ${SMTP_PASSWORD}' < /config.php.template > /var/www/html/include/config.php
 # Start Apache in the foreground (keeps the container alive)
 apachectl -D FOREGROUND
