c5dc8adaf1d52f038d033971327377f76eda428e
Tilo Thiele <tilo.thiele@emessages.de>
2026-06-13 23:18:44 +0200

    pwa

--- hzd-pwa/src/app/(authenticated)/wurfabnahmen/[id]/page.tsx
+++ hzd-pwa/src/app/(authenticated)/wurfabnahmen/[id]/page.tsx
@@ -1,16 +1,29 @@
-import { WurfabnahmeEditor } from '@/components/wurfabnahme/WurfabnahmeEditor'
+'use client'
 
-export default async function WurfabnahmeDetailPage({
-	params,
-}: {
-	params: Promise<{ id: string }>
-}) {
-	const { id } = await params
+import { WurfabnahmeEditor } from '@/components/wurfabnahme/WurfabnahmeEditor'
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+export default function WurfabnahmeDetailPage() {
+	const params = useParams()
+	const router = useRouter()
+	const id = typeof params.id === 'string' ? params.id : params.id?.[0]
+	const [recordId, setRecordId] = useState<string | undefined>(undefined)
+
+	useEffect(() => {
+		if (id) {
+			setRecordId(id)
+		}
+	}, [id])
+
+	if (!recordId) {
+		return null
+	}
 
 	return (
 		<WurfabnahmeEditor
-			basePath={`/wurfabnahmen/${id}`}
-			recordId={id}
+			basePath={`/wurfabnahmen/${recordId}`}
+			recordId={recordId}
 		/>
 	)
 }
